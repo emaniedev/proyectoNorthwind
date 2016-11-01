e04c80a623a155cd305b42e72809f3140a810185
manie <manie@MAZHE-PC>
2016-11-01 17:04:31 +0100

    Primer commit

---
+++ Proyecto_Tutatu/tutatu/tutatu/Models/EFwebuser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using tutatu.Models;
+
+namespace tutatu.Models
+{
+    public class EFwebuser
+    {
+        private tutatuEF datos = new tutatuEF();
+
+
+
+        public void crearWebUser (string nick, string pass, string nom, string ape, string email, DateTime bday, string sexo)
+    {
+            usuarios nuevoUser = new usuarios();
+            webuser nuevo = new webuser();
+            DateTime hoy = DateTime.Now;
+            nuevoUser.nickname = nick;
+            nuevoUser.pass1 = pass;
+            nuevoUser.pass2 = pass;
+            nuevoUser.date_reg = hoy;
+            nuevo.fname = nom;
+            nuevo.sname = ape;
+            nuevo.email = email;
+            nuevo.b_date = bday;
+            nuevo.sexo = sexo;
+            nuevoUser.webuser.Add(nuevo);
+            
+
+            try
+            {
+                datos.usuarios.Add(nuevoUser);
+                
+                
+                datos.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+
+            
+    }
+
+}
+  
+}
