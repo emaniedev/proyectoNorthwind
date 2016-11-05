e13ea64c583f513a77d184d65185b4e41e3db14b
manie <manie@MAZHE-PC>
2016-11-05 02:18:01 +0100

    Añadidas funciones esteticas en la navbar 05/11/2046

---
+++ Proyecto_Tutatu/tutatu/tutatu/Controllers/VideosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tutatu.Controllers
+{
+    public class VideosController : Controller
+    {
+        // GET: Videos
+        public ActionResult Index()
+        {
+            ViewBag.ACTIVO = "video";
+            return View();
+        }
+    }
+}
---
+++ Proyecto_Tutatu/tutatu/tutatu/scripts/navbar.js
@@ -0,0 +1,7 @@
+﻿/***
+    Script para hacer que se cambie el foco de el navbar al pasar a distintas
+    secciones.
+*/
+
+function navActive (name){
+    
